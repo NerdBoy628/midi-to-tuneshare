3263a7ad358dde3574a3e774efc5751d36532743
NerdBoy628 <nerdboy628@gmail.com>
2025-01-08 21:01:55 -0500

    Update main.py
    Added support for track volume changes and tried to fix the rounding issues

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # —— CONVERT MIDI FILE TO TUNESHARE CODE ——
-# This version has a bug where rounding errors cause beats to be split incorrctly,
-# sometimes skipping beats and sometimes adding extras.
-# I don't know how to fix this yet. Please help 😩
+# You may have to mess around with the `multiplier` variable
+# until it works. When the list of numbers it prints is mostly
+# whole numbers, it should sound fine in TuneShare.
 
 path = 'enter path here'
 multiplier = 16
+tempo_changes = False
 
 inst_table = "00001144ffgfiif42227997734543444544544kk7777765h77dbebd7888a87jkaaaa88d9dbdcccddkjcb4cbkgdkcd49bfdf3edd44464fa79f6hllllllcll9llll"
 drum_table = [18,24,25,25,18,17,20,21,11,11,12,10,17,10,23,15,23,24,23,13,22,22,13,22,14,13,13,16,14,19,13,26,14,20,20,17,22,23,10,10,27,27,15,15,16,16,25,25,18,20,22,27,27,16,21,24,14,16,17,11,11]
@@ -14,18 +15,26 @@ import os, pyperclip, time
 os.system('clear')
 
 mid = MidiFile(path)
+firstbpm = 0
+for msg in mid:
+    if msg.type == 'set_tempo':
+        firstbpm = 60000000 / msg.tempo
+        bpm = firstbpm
+        break
 channels = [0]*16
+volumes = [0]*16
 note_on_times = {}
 data = []
-code = str(int(mid.ticks_per_beat*multiplier/32))+"!"
-clock = 0
+code = str(int(firstbpm*multiplier/4))+"!"
+clock = 0.0
 errors = 0
 for msg in mid:
-    clock += msg.time
+    clock += msg.time*firstbpm/bpm
+    print(clock*multiplier)
     if msg.type == 'program_change':
         channels[msg.channel] = 128 if msg.channel == 9 else msg.program
     elif msg.type == 'note_on' and 10 <= msg.note <= 99:
-            data.append([msg.note-14,channels[msg.channel],1,msg.velocity,clock])
+            data.append([msg.note-14,channels[msg.channel],1,msg.velocity*volumes[msg.channel]/127,clock])
             note_on_times[(msg.channel, msg.note)] = [clock,len(data)]
     elif msg.type == 'note_off':
         if (msg.channel, msg.note) in note_on_times:
@@ -33,7 +42,15 @@ for msg in mid:
             del note_on_times[(msg.channel, msg.note)]
         else:
             errors += 1
+    elif msg.type == 'tempo':
+        if not tempo_changes:
+            bpm = 60000000 / msg.tempo
+    elif msg.type == 'control_change':
+        if msg.control == 7:
+            volumes[msg.channel] = msg.value
+print("Errors: "+str(errors))
 clock = 0
+
 for i in data:
     i[2] = round(i[2]*multiplier)
     i[4] = round(i[4]*multiplier)
