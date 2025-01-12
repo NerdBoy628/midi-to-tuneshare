776de40a8eda7f22820e6757004c1fc4e87c699f
NerdBoy628 <nerdboy628@gmail.com>
2025-01-12 10:20:07 -0500

    Fix tempo issues
    Finally figured out why there were so many rounding errors! It should work fine now (I think)

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
-# —— CONVERT MIDI FILE TO TUNESHARE CODE ——
-# You may have to mess around with the `multiplier` variable
-# until it works. When the list of numbers it prints is mostly
-# whole numbers, it should sound fine in TuneShare.
+# CONVERT MIDI FILE TO TUNESHARE CODE
+# `multiplier` is the accuracy of the song, 
+# you may have to play around with the value to get desired results.
+# `tempo_changes` determines if it tries to approximate tempo changes.
+# It ignores every track in `disabled_tracks`
 
 path = 'enter path here'
-multiplier = 16
+multiplier = 8
 tempo_changes = False
+disabled_tracks = []
 
 inst_table = "00001144ffgfiif42227997734543444544544kk7777765h77dbebd7888a87jkaaaa88d9dbdcccddkjcb4cbkgdkcd49bfdf3edd44464fa79f6hllllllcll9llll"
 drum_table = [18,24,25,25,18,17,20,21,11,11,12,10,17,10,23,15,23,24,23,13,22,22,13,22,14,13,13,16,14,19,13,26,14,20,20,17,22,23,10,10,27,27,15,15,16,16,25,25,18,20,22,27,27,16,21,24,14,16,17,11,11]
 
-from mido import MidiFile
-import os, pyperclip, time
+from mido import MidiFile, second2tick
+import os, pyperclip
 os.system('clear')
+
+def sec2tick(x):
+    return(second2tick(x,mid.ticks_per_beat,60000000/bpm)/mid.ticks_per_beat)
 
 mid = MidiFile(path)
 firstbpm = 0
 for msg in mid:
     if msg.type == 'set_tempo':
-        firstbpm = 60000000 / msg.tempo
+        firstbpm = round(60000000 / msg.tempo)
         bpm = firstbpm
         break
 channels = [0]*16
-volumes = [0]*16
+volumes = [127]*16
 note_on_times = {}
 data = []
 code = str(int(firstbpm*multiplier/4))+"!"
 clock = 0.0
-errors = 0
+tempo_change_count = 0
 for msg in mid:
-    clock += msg.time*firstbpm/bpm
-    print(clock*multiplier)
+    clock += sec2tick(msg.time)*firstbpm/bpm
     if msg.type == 'program_change':
         channels[msg.channel] = 128 if msg.channel == 9 else msg.program
-    elif msg.type == 'note_on' and 10 <= msg.note <= 99:
+    elif msg.type == 'note_on' and 10 <= msg.note <= 99 and msg.velocity > 0 and msg.channel not in disabled_tracks:
             data.append([msg.note-14,channels[msg.channel],1,msg.velocity*volumes[msg.channel]/127,clock])
             note_on_times[(msg.channel, msg.note)] = [clock,len(data)]
-    elif msg.type == 'note_off':
+    elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
         if (msg.channel, msg.note) in note_on_times:
             data[note_on_times[(msg.channel, msg.note)][1]-1][2] = clock - note_on_times[(msg.channel, msg.note)][0]
             del note_on_times[(msg.channel, msg.note)]
-        else:
-            errors += 1
     elif msg.type == 'tempo':
         if not tempo_changes:
-            bpm = 60000000 / msg.tempo
+            bpm = round(60000000 / msg.tempo)
+            tempo_change_count += 1
     elif msg.type == 'control_change':
         if msg.control == 7:
             volumes[msg.channel] = msg.value
-print("Errors: "+str(errors))
-clock = 0
+print("Tempo changes: "+str(tempo_change_count))
 
+clock = 0
 for i in data:
     i[2] = round(i[2]*multiplier)
     i[4] = round(i[4]*multiplier)
